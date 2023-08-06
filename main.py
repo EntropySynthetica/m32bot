680b12d1d4c4627470b529fb180d6450e4b208dd
Erica Zavaleta <44880722+EntropySynthetica@users.noreply.github.com>
2023-08-06 15:33:02 -0500

    Fixing indentiation

--- main.py
+++ main.py
@@ -27,20 +27,20 @@ def log(s):
 
 # Func to send this text to the M32
 def sendmoppstr(adr, txtstr):
-        log(txtstr)
-        sendstr = translator.txttomopp(txtstr)
-        splits = [sendstr[x:x + 8] for x in range(0, len(sendstr), 8)]
-        frame = bytearray()
-        for split in splits:
-                value = 0
-                t = len(split)
-                for i in range (t): 
-                        value *= 2   # // double the result so far
-                        if split[i] == '1':
-                                value +=1 #++; //add 1 if needed
-                frame.append(value)  #
-        time.sleep(0.2)
-        sock.sendto(frame,adr)
+    log(txtstr)
+    sendstr = translator.txttomopp(txtstr)
+    splits = [sendstr[x:x + 8] for x in range(0, len(sendstr), 8)]
+    frame = bytearray()
+    for split in splits:
+        value = 0
+        t = len(split)
+        for i in range (t): 
+            value *= 2   # // double the result so far
+            if split[i] == '1':
+                value +=1 #++; //add 1 if needed
+        frame.append(value)  #
+    time.sleep(0.2)
+    sock.sendto(frame,adr)
 
 
 # Generate a random US format Callsign
@@ -48,9 +48,9 @@ def makeCall():
     prefixA = random.choice(['a','k','w','n'])
     prefixB = ""
     if random.randint(1,2) == 1:
-          prefixB = ''.join(random.choices(string.ascii_lowercase, k=1))
-    dist = ''.join(random.choices(string.digits, k=1))
-    suffix = ''.join(random.choices(string.ascii_lowercase, k=random.randint(1, 3)))
+        prefixB = ''.join(random.choices(string.ascii_lowercase, k=1))
+        dist = ''.join(random.choices(string.digits, k=1))
+        suffix = ''.join(random.choices(string.ascii_lowercase, k=random.randint(1, 3)))
 
     botCall = prefixA + prefixB + dist + suffix
     
@@ -74,7 +74,7 @@ def main():
         now = int(time.time())
         lastRxAge = now - lastRxTime
         if lastRxAge > 8:
-              rxBuffer = ""
+            rxBuffer = ""
         
 
         # Add the last recieved character or characters from the M32 to a buffer.
@@ -85,17 +85,17 @@ def main():
         # If we send the word ping send back the word pong to verify everything is working.  
         pingRex = r'^ping'
         if re.match(pingRex, rxBuffer.replace(" ", "")):
-              print("TX: pong")
-              sendmoppstr(client_address, 'pong')
-              rxBuffer = ""
+            print("TX: pong")
+            sendmoppstr(client_address, 'pong')
+            rxBuffer = ""
 
 
         # If we get the word test send back a random US call. 
         testRex = r'^test'
         if re.match(testRex, rxBuffer.replace(" ", "")):
-              sendmoppstr(client_address, makeCall())
-              print("TX: Bot Call")
-              rxBuffer = ""
+            sendmoppstr(client_address, makeCall())
+            print("TX: Bot Call")
+            rxBuffer = ""
 
 
         lastRxTime = int(time.time())
